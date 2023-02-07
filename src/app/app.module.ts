8da9ea59ea2f6ce8aa1d03d15bd838bc5471c4dc
HelenMayer <you@example.com>
2023-02-07 12:38:53 +0200

    add warning and success  project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  faSkullCrossbones = faSkullCrossbones;
+
+}
